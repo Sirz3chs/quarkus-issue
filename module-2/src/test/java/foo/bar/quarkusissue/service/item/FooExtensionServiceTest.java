f4b13cb7a09f984f214f196b152ca19b7ad7d4db
BOIS Pierre-Antoine <pierre-antoine.bois@hifi-filter.com>
2021-12-07 14:01:05 +0100

    rename test class
    # Body
    #<body>
    
    # Footer
    #BREAKING-CHANGE: <desc>
    
    # ================================================
    # HEADER (mandatory)
    # ================================================
    #
    # Type (mandatory, enum)
    # ======================
    # - build: Changes that affect the build system or external dependencies
    # - chore: Misc (.gitignore...)
    # - ci: Changes to CI configuration files and scripts
    # - data: Migration scripts for DB
    # - docs: Documentation only changes
    # - feat: A new feature
    # - fix: A bug fix
    # - perf: A code change that improves performance
    # - refactor: A code change that neither fixes a bug nor adds a feature
    # - style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
    # - test: Adding missing tests or correcting existing tests
    # - release: Bump version
    # - revert: Revert some changes (subject = same as reverting commit. Body = "This reverts commit <hash>.")
    #
    # See https://github.com/angular/angular/blob/master/CONTRIBUTING.md#type
    #
    #
    # Scope (optional, enum)
    # ======================
    # - core
    # - common
    # - customer
    # - supplier
    # - ...
    #
    # Subject (mandatory)
    # ====================
    # - use the imperative, present tense: "change" not "changed" nor "changes"
    # - don't capitalize the first letter
    # - no dot (.) at the end
    #
    # Issue (mandatory)
    # =================
    # Issue number (can be multiple)
    #
    #
    # ================================================
    # BODY (optional)
    # ================================================
    # - full explainations
    # - use the imperative, present tense: "change" not "changed" nor "changes"
    # - capitalize the first letter and add a dot at the end
    #
    # ================================================
    # FOOTER (optional)
    # ================================================
    # - Add breaking change info with "BREAKING CHANGE: <desc>" (optional)
    #
    # ================================================
    # EXAMPLES
    # ================================================
    #
    # Simple feature
    # ==============
    #
    # feat(excel-generator): allow starting at row 1 #123
    #
    #
    # Complex feature
    # ===============
    #
    # feat(supplier): add the supplier notifications #123 #456
    #
    # Send notifications to the specified LDAP group users when a new supplier is created.
    #
    #
    # Fix + no scope + Breaking change
    # ================================
    #
    # fix: fix API authentication #56984
    #
    # Clients are now required to provide a token in the "HIFI-auth" HTTP header.
    #
    # BREAKING-CHANGE: All clients have to provide the "HIFI-auth" HTTP header to call the API. 
    #
    #
    # Revert
    # ================================
    # revert: fix API authentication #56984
    #
    # This reverts commit <hash>.
    #
    #
    # Release
    # ================================
    # release: v3.0.0

--- module-2/src/test/java/foo/bar/quarkusissue/service/item/FooExtensionServiceTest.java
+++ module-2/src/test/java/foo/bar/quarkusissue/service/item/FooExtensionServiceTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
-public class ItemExtensionServiceTest {
+public class FooExtensionServiceTest {
 
     @Inject
     @Child
